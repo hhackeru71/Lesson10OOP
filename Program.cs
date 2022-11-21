63effbb60358144b97b27d6205620d9ee80fc760
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-21 18:46:05 +0200

    .

--- Program.cs
+++ Program.cs
@@ -4,15 +4,56 @@
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();//
-            list.Add(1);
+            //ליצור Dictionary 
+            //מפתח Int  וגם ה value  int
+            //להוסיף 5 ערכים 
+            //להדפיס בלולאה 
+
+
+
+            Dictionary<int,string> listD = new Dictionary<int, string>();
+            Dictionary<int, string> list1 = null;
+            list1 = listD;
+            listD.Add(1, "Moshe");
+            listD.Add(2, "sdfsdf");
+            Console.WriteLine(list1[1]);
+            //במידה ורוצים לשנות את הליסט נשנה לפי ה key
+            listD[1] = "yarin";
+            //Console.WriteLine(listD[1]);
+            
+            //foreach (var item in listD)
+            //{
+            //    //Console.WriteLine(item);
+            //}
+            
+
+
+
+            //ליצור רשימה מסוכ string 
+            //להוסיף לרשימה 3 ערכים 
+            //באינדקס האחרון של הרשימה  צריך למחוק אותו 
+            //לאחר מכן להדפיס את כל הרשימה
+
+            var a = 1;
+
+            int num = 0;
+            object obj1 = 0;//boxing unboxing שאלה מתוך ראיון עבודה
+            object obj2 = "sdf";
+            obj1 = obj2;
+           
+            int num2 =(int)obj1;
+
+            //List<object> list1 = new List<object>();//הגדרת list list  של object
+            List<int> list = new List<int>();//הגדרת list
+            list.Add(1);//הוספת ערכים llist
             list.Add(2);
             list.Add(3);
             //list[0] = 4;
-            list.RemoveAt(1);
+            list.Remove(3);
+            //list.RemoveAt(1);//מחיקה ע"י האינדקס
             foreach (var item in list)
             {
-                Console.Write(item + " ");
+                //Console.Write(item + " ");
 
             }
 
