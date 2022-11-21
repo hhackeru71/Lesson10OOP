47de657d69206ffa4ac15852a9153ad281636806
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-21 17:47:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson10OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();//
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            //list[0] = 4;
+            list.RemoveAt(1);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+
+            }
+
+        }
+    }
+}
