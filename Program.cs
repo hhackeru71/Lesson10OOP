b5eea6b81aeb8d2f13aad6879ca63bb7a2f51f63
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-21 19:14:17 +0200

    .

--- Program.cs
+++ Program.cs
@@ -19,59 +19,63 @@ namespace Lesson10OOP
             // y =ערך כלשהו
             //להדפיס את x y 
 
-            Point<string> point = new Point<string>();
-            //Point<int> point1 = new Point<int>();
-            point.X = "sdfsdf";
+            Students<string,string> students = new Students<string,string>();
+            students.X = "sdfdsf";
+            students.Y = "sdfsdfsdf";
+            Console.WriteLine($"{students.X} {students.Y} ");
+            //Point<string> point = new Point<string>();
+            ////Point<int> point1 = new Point<int>();
+            //point.X = "sdfsdf";
 
 
-            //ליצור Dictionary 
-            //מפתח Int  וגם ה value  int
-            //להוסיף 5 ערכים 
-            //להדפיס בלולאה 
+            ////ליצור Dictionary 
+            ////מפתח Int  וגם ה value  int
+            ////להוסיף 5 ערכים 
+            ////להדפיס בלולאה 
 
-            Dictionary<int,string> listD = new Dictionary<int, string>();
-            Dictionary<int, string> list1 = null;
-            list1 = listD;
-            listD.Add(1, "Moshe");
-            listD.Add(2, "sdfsdf");
-            Console.WriteLine(list1[1]);
-            //במידה ורוצים לשנות את הליסט נשנה לפי ה key
-            listD[1] = "yarin";
-            //Console.WriteLine(listD[1]);
+            //Dictionary<int,string> listD = new Dictionary<int, string>();
+            //Dictionary<int, string> list1 = null;
+            //list1 = listD;
+            //listD.Add(1, "Moshe");
+            //listD.Add(2, "sdfsdf");
+            //Console.WriteLine(list1[1]);
+            ////במידה ורוצים לשנות את הליסט נשנה לפי ה key
+            //listD[1] = "yarin";
+            ////Console.WriteLine(listD[1]);
             
-            //foreach (var item in listD)
-            //{
-            //    //Console.WriteLine(item);
-            //}
+            ////foreach (var item in listD)
+            ////{
+            ////    //Console.WriteLine(item);
+            ////}
             
 
-            //ליצור רשימה מסוכ string 
-            //להוסיף לרשימה 3 ערכים 
-            //באינדקס האחרון של הרשימה  צריך למחוק אותו 
-            //לאחר מכן להדפיס את כל הרשימה
+            ////ליצור רשימה מסוכ string 
+            ////להוסיף לרשימה 3 ערכים 
+            ////באינדקס האחרון של הרשימה  צריך למחוק אותו 
+            ////לאחר מכן להדפיס את כל הרשימה
 
-            var a = 1;
+            //var a = 1;
 
-            int num = 0;
-            object obj1 = 0;//boxing unboxing שאלה מתוך ראיון עבודה
-            object obj2 = "sdf";
-            obj1 = obj2;
+            //int num = 0;
+            //object obj1 = 0;//boxing unboxing שאלה מתוך ראיון עבודה
+            //object obj2 = "sdf";
+            //obj1 = obj2;
            
-            int num2 =(int)obj1;
+            //int num2 =(int)obj1;
 
-            //List<object> list1 = new List<object>();//הגדרת list list  של object
-            List<int> list = new List<int>();//הגדרת list
-            list.Add(1);//הוספת ערכים llist
-            list.Add(2);
-            list.Add(3);
-            //list[0] = 4;
-            list.Remove(3);
-            //list.RemoveAt(1);//מחיקה ע"י האינדקס
-            foreach (var item in list)
-            {
-                //Console.Write(item + " ");
+            ////List<object> list1 = new List<object>();//הגדרת list list  של object
+            //List<int> list = new List<int>();//הגדרת list
+            //list.Add(1);//הוספת ערכים llist
+            //list.Add(2);
+            //list.Add(3);
+            ////list[0] = 4;
+            //list.Remove(3);
+            ////list.RemoveAt(1);//מחיקה ע"י האינדקס
+            //foreach (var item in list)
+            //{
+            //    //Console.Write(item + " ");
 
-            }
+            //}
 
         }
     }
---
+++ Students.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10OOP
+{
+    internal class Students<T,V>
+    {
+        public T X { get; set; }
+        public V Y { get; set; }
+
+     
+    }
+}
